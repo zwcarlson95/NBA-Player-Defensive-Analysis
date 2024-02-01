ef40030d5d7a7f6a8a7e30dc44e926d830b8c4fc
Zechariah Carlson <zachcarlson@comcast.net>
2024-02-01 15:51:26 -0700

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-team_stats = pd.read_csv('/Users/zachcarlson/Documents/NBA Project/2021-22/team_stats22')
-player_stats = pd.read_csv('/Users/zachcarlson/Documents/NBA Project/2021-22/player_stats22.csv')
-hustle_stats = pd.read_csv('/Users/zachcarlson/Documents/NBA Project/2021-22/hustle_stats22.csv')
+team_stats = pd.read_csv('team_stats22')
+player_stats = pd.read_csv('player_stats22.csv')
+hustle_stats = pd.read_csv('hustle_stats22.csv')
 
 player_merged = pd.merge(player_stats, hustle_stats, on='PLAYER_ID', how='inner')
 
@@ -13,7 +13,6 @@ for col in player_merged.columns:
     if col not in exclude_columns:
         player_merged[col + '_PG'] = player_merged.apply(lambda x: x[col] / x['GP'] if x['GP'] > 0 else 0, axis=1)
 
-# Assuming your per game columns end with '_per_game'
 per_game_columns = [col for col in player_merged.columns if '_PG' in col]
 
 # Round these specific columns to 2 decimal places
@@ -23,4 +22,4 @@ player_merged = player_merged.drop(columns=['GP', 'MIN', 'BLK', 'BLKA', 'STL', '
                            'CONTESTED_SHOTS_3PT', 'CONTESTED_SHOTS', 'DEFLECTIONS', 'LOOSE_BALLS_RECOVERED',
                            'CHARGES_DRAWN'])
 
-player_merged.to_csv('/Users/zachcarlson/Documents/NBA Project/2021-22/player_merged_stats22.csv', index=False)
+player_merged.to_csv('player_merged_stats22.csv', index=False)
